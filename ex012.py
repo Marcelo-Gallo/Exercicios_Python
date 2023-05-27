b29678815ddc952803aaf82440988c980e40100a
Marcelo-Gallo <marceloaugustogallo@gmail.com>
2023-05-27 11:15:51 -0300

    ex 12/13

--- ex012.py
+++ ex012.py
@@ -1,1 +1,14 @@
 #Faça um algoritmo que leia o preço de um produto e mostre seu novo preço, com 5% de desconto.
+
+#variaveis
+desconto = 0.0
+preco = 0.0
+npreco = 0.0
+
+#algoritmo
+
+preco = float(input("Informe qual o preço do produto: R$"))
+desconto = float(input("Informe qual o desconto que deseja aplicar em %: "))
+desconto = (desconto/100)
+npreco = preco - (preco*desconto)
+print(f"O novo preço do produto com o desconto aplicado é R${npreco}")
--- ex013.py
+++ ex013.py
@@ -1,1 +1,12 @@
 #Faça um algoritmo que leia o salário de um funcionário e mostre seu novo salário, com 15% de aumento.
+
+#variaveis: 
+sal = 0.0
+nsal = 0.0
+aumento = 0.0
+
+sal = float(input("Informe o salário do funcionário: R$"))
+aumento = float(input("Informe o percentual de aumento do salário: "))
+aumento = aumento/100
+nsal = sal + (sal*aumento)
+print(f"O salário com o aumento é: R${nsal}")
