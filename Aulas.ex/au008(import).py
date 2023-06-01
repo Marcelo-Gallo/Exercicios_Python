e693ed5d3c00ff022ff53f0ad0b423948782144c
Marcelo-Gallo <marceloaugustogallo@gmail.com>
2023-06-01 20:55:00 -0300

    ex 14 ao 21 curso em video

---
+++ Aulas.ex/au008(import).py
@@ -0,0 +1,11 @@
+'''
+import math --> biblioteca toda
+from math import sqrt --> importando sqrt (raiz quadrada de math)
+https://docs.python.org/3/library/index.html --> bibliotecas :3
+ctrl + espaço (depois de 'import ') --> abre uma lista de bibliotecas.
+'''
+import math
+
+num =int(input('Digite um numero: '))
+raiz = math.sqrt(num)
+print(f'A raiz de {num} é igual a {raiz}')
