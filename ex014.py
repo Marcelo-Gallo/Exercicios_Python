e0c85b5abdeb87e7b23737ce91c60784d72da756
Marcelo-Gallo <marceloaugustogallo@gmail.com>
2023-06-04 14:08:13 -0300

    ex14

--- ex014.py
+++ ex014.py
@@ -1,1 +1,7 @@
 #Escreva um programa que converta uma temperatura digitando em graus Celsius e converta para graus Fahrenheit.
+import math
+
+c = float(input("Informe a temperatira em °C: "))
+f = (c * 1.8)+32
+
+print(f"A temperatura de {c}°C é equivalente à {f}°F!")
